93ff55059caa0285c381519b77a7a79029b9b947
Ceyhun Alar <cey_cey_ceyhun@hotmail.com>
2022-11-16 13:56:20 +0100

    Added exportLatent function

--- clustering/clustering.py
+++ clustering/clustering.py
@@ -3,6 +3,25 @@ import scanpy as sc
 import anndata as ad
 from typing import Tuple
 
+def exportLatent(adata:ad.AnnData,latent_key:str) -> ad.AnnData:
+    """Takes anndata and latent key as input and returns a new anndata object of the specified latent space with the metadata of the original object
+
+    Parameters
+    ----------
+    adata : ad.AnnData
+        adata
+    latent_key : str
+        latent_key
+
+    Returns
+    -------
+    ad.AnnData
+
+    """
+    bdata = ad.AnnData(adata.obsm[latent_key])
+    addMetadataFromPandas(bdata,adata.obs)
+    return bdata
+
 def subsample(adata:ad.AnnData, cluster_key:str, target_cells:float=10000) -> ad.AnnData:
     """ Takes as input anndata object and will subsample target cells per cluster based on cluster key. If no cluster key is given will subsample 20% of entire anndata. 
 
@@ -52,7 +71,7 @@ def clusterLatentspace(adata:ad.AnnData,cluster_key=None,show_plot:bool=False):
     if show_plot:
         sc.pl.umap(adata,color=cluster_key,size=12)
         
-def plotVariableLatent(adata:ad.AnnData,cluster_key='leiden',method='wilcoxon',num_features=5,variable_names=adata.var_names) -> Tuple:
+def plotVariableLatent(adata:ad.AnnData,cluster_key='leiden',method='wilcoxon',num_features=5,variable_names=[]) -> Tuple:
     """
     Takes as input AnnData and plots a dotplot of the cluster_key depending on the variable features. Methods can be chosen to rank variable genes between groups to return in a dataframe.
     Parameters
@@ -69,10 +88,11 @@ def plotVariableLatent(adata:ad.AnnData,cluster_key='leiden',method='wilcoxon',n
         Variable names for plotting on the dotplot
     Returns
     -------
-    DotPlot of the different clusters
     DataFrame showing cluster_key and its most variable num_features 
     """
-    P1 = sc.pl.dotplot(adata, var_names=variable_names, groupby=cluster_key)
+    if not variable_names:
+        variable_names=adata.var_names
+    sc.pl.dotplot(adata, var_names=variable_names, groupby=cluster_key)
     sc.tl.rank_genes_groups(adata, groupby=cluster_key, method=method, key_added=method)
     var_features = pd.DataFrame(adata.uns[method]['names']).head(num_features)
-    return P1,var_features 
+    return var_features 
